2fcb51700d87aa7e94802bc8732fb518ab0bd424
Cahyo <cariercahyo@gmail.com>
2021-05-22 19:36:55 +0800

    #

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app;
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Starting server development: https://localhost:${PORT}`);
